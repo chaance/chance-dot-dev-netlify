bd8eb4bf775abba632f69ecf4e259d38e94530f6
Chance Strickland <hi@chance.dev>
2022-04-10 11:29:55 -0700

    minify HTML + update remix

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
 import { renderToString } from "react-dom/server";
 import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
+import * as minifyHtml from "@minify-html/js";
 
 const FUN_ASCII = `
 <!--
@@ -14,6 +15,17 @@ const FUN_ASCII = `
 -->
 `;
 
+const minifyHtmlConfig = minifyHtml.createConfiguration({
+	do_not_minify_doctype: true,
+	ensure_spec_compliant_unquoted_attribute_values: true,
+	keep_closing_tags: true,
+	keep_comments: false,
+	keep_html_and_head_opening_tags: true,
+	keep_spaces_between_attributes: true,
+	minify_css: true,
+	minify_js: true,
+});
+
 export default function handleRequest(
 	request: Request,
 	responseStatusCode: number,
@@ -26,8 +38,15 @@ export default function handleRequest(
 
 	responseHeaders.set("Content-Type", "text/html");
 
-	return new Response("<!DOCTYPE html>\n" + FUN_ASCII + markup, {
-		status: responseStatusCode,
-		headers: responseHeaders,
-	});
+	return new Response(
+		"<!DOCTYPE html>\n" +
+			FUN_ASCII +
+			(process.env.NODE_ENV === "production"
+				? minifyHtml.minify(markup, minifyHtmlConfig).toString()
+				: markup),
+		{
+			status: responseStatusCode,
+			headers: responseHeaders,
+		}
+	);
 }
